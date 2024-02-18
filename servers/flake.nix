afe9a71b9eb21730acac3bb30ed95ac08c19235d
blue linden <dev@bluelinden.art>
2024-02-18 01:30:55 -0500

    initial commit

--- servers/flake.nix
+++ servers/flake.nix
@@ -11,7 +11,39 @@ rec {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
-          { system.build.diskoScript = ./disk-config.nix; }
+          { system.build.diskoScript = {
+            disko.devices = {
+              disk = {
+                sda = {
+                  device = "/dev/sda";
+                  type = "disk";
+                  content = {
+                    type = "gpt";
+                    partitions = {
+                      ESP = {
+                        end = "500M";
+                        type = "EF00";
+                        content = {
+                          type = "filesystem";
+                          format = "vfat";
+                          mountpoint = "/boot";
+                        };
+                      };
+                      root = {
+                        name = "root";
+                        end = "-0";
+                        content = {
+                          type = "filesystem";
+                          format = "btrfs";
+                          mountpoint = "/";
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          }; }
           ./barbarian.configuration.nix
         ];
         specialArgs = {
